b233031c6f70fc7143f4bbb4a2e27d132d5deb82
raylu15742cs <raylucs15742@gmail.com>
2022-09-06 15:09:10 -0700

    add 15% calculator

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const itstime = () => {
     let input1 = document.createElement("input")
     input1.type = "number";
     let button1 = document.createElement("button")
-    button1.innerHTML = 'Calculate';
+    button1.innerHTML = '10%';
     cool.appendChild(input1)
     cool.appendChild(button1);
     button1.addEventListener("click", function() {
@@ -22,6 +22,24 @@ const itstime = () => {
             );
         }
     })
+    let button2 = document.createElement("button")
+    button2.innerHTML = "15%";
+    button2.addEventListener('click', function () {
+      if (input1.value > 1) {
+        console.log(input1.value);
+        let new1 = Math.ceil(parseFloat(input1.value * 1.15));
+        let tip = parseFloat(new1 - input1.value).toFixed(2);
+        let final = document.createElement('p');
+        final.innerHTML = `Tips : ${tip} Total : ${new1}`;
+        cool.appendChild(final);
+        console.log(
+          new1,
+          input1.value,
+          parseFloat(new1 - input1.value).toFixed(2)
+        );
+      }
+    });
+    cool.appendChild(button2)
 }
 
 
